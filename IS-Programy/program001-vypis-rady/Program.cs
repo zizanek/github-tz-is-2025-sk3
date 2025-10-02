3a4f7fcd4f840c7e762ab273797b77b3fee56129
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-02 17:30:45 +0200

    #3 Základ programu pro výpis řady - vstup pro první číslo

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,43 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("************* Výpis číselné řady *************");
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("**************** Tomáš Žižka *****************");
+    Console.WriteLine("**************** 02.10.2025 ******************");
+    Console.WriteLine("**********************************************");
+    Console.WriteLine();
+
+    // Vstup číselné hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());  
+
+
+    // Vstup číselné hodnoty do programu - lépe řešený
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+
+
+}
+
+
