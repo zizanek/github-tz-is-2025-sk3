2d106af7fd065e88e6c2dfcf929f513922e91dba
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-16 16:28:49 +0200

    #3 Výpis řady - vstup

--- IS-Programy/program001-vypis-rady/Program.cs
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -24,6 +24,25 @@ while (again == "a")
     }
 
 
+    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+    int last;
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
+
+    Console.Write("Zadejte diferenci (celé číslo): ");
+    int step;
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte diferenci znovu: ");
+    }
+
+
+    
+
+
+
 
 
 
