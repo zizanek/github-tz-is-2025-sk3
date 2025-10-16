eb85415dfdb59e18411674e6094f0315a76ac68c
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-16 17:32:08 +0200

    #4 DONE ale bez součinu

--- IS-Programy/program002a-soucet-cifer/Program.cs
+++ IS-Programy/program002a-soucet-cifer/Program.cs
@@ -3,37 +3,50 @@ string again = "a";
 while (again == "a")
 {
     Console.Clear();
-    Console.WriteLine("**********************************************");
-    Console.WriteLine("************* Název programu *************");
-    Console.WriteLine("**********************************************");
-    Console.WriteLine("**************** Tomáš Žižka *****************");
+    Console.WriteLine("*************************************************");
+    Console.WriteLine("************* Součet a součin cifer *************");
+    Console.WriteLine("*************************************************");
+    Console.WriteLine("**************** Tomáš Žižka ********************");
     Console.WriteLine("**************** Aktuální datum ******************");
-    Console.WriteLine("**********************************************");
+    Console.WriteLine("**************************************************");
     Console.WriteLine();
 
     // Vstup hodnoty do programu
-    Console.Write("Zadejte první číslo řady (celé číslo): ");
-    int first;
-    while (!int.TryParse(Console.ReadLine(), out first))
+    Console.Write("Zadejte celé číslo: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+        Console.Write("Nezadali jste celé číslo. Zadejte ho znovu: ");
     }
 
+    int suma = 0;
+    int numberBackup = number;
+    int digit;
 
+    if (number < 0)
+    {
+        number = -number;
+    }
 
+    while (number >= 10)
+    {
+        digit = number % 10;
+        number = (number - digit) / 10;
+        Console.WriteLine("Zbytek = {0}", digit);
+        suma = suma + digit;
+    }
+    
+    Console.WriteLine("Poslední zbytek: {0}", number);
 
+    // musíme přičíst ještě poslední cifru
+    suma = suma + number;
 
-
-
-
-
+    Console.WriteLine();
+    Console.WriteLine("Součet cifer čísla {0} je {1}", numberBackup, suma);
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
     again = Console.ReadLine();
-
-
-
 }
 
 
