19e818ae6974d5e5975e9228831b3494a79c4805
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-16 16:20:09 +0200

    #32 Zakladni kod

---
+++ IS-Programy/program000a-zakladni-kod/Program.cs
@@ -0,0 +1,38 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("************* Název programu *************");
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("**************** Tomáš Žižka *****************");
+    Console.WriteLine("**************** Aktuální datum ******************");
+    Console.WriteLine("**********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+
+
+}
+
+
