e26e9431ad25cd63d7922da8c4c517d4b55866f6
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-16 17:07:26 +0200

    #4 základní kód

--- IS-Programy/program002a-soucet-cifer/Program.cs
+++ IS-Programy/program002a-soucet-cifer/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("************* Název programu *************");
+    Console.WriteLine("**********************************************");
+    Console.WriteLine("**************** Tomáš Žižka *****************");
+    Console.WriteLine("**************** Aktuální datum ******************");
+    Console.WriteLine("**********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+
+
+}
+
+
